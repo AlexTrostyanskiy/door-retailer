8da9c52ccfad28d72650870dd86f30095437f799
slawiko <shchaurouski.slava@gmail.com>
2024-03-11 12:31:08 +0100

    Removes redundant tailwind color extension

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
   theme: {
     extend: {
-      colors: { ashy: "#fdfbfb" }
+
     }
   },
   plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography"), require("@tailwindcss/aspect-ratio")]
